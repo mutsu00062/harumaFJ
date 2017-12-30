b7b230ee858cc63f5a9101441ceda19e6b5ed950
mutsu00062 <mutsu00062@gmail.com>
2017-12-31 00:49:45 +0900

    no comme

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ foreach ($events as $event) {
             $date = fgets($fp);
             $key = fgets($fp);
             $date = rtrim($date);
-            $date = (double)$date;
+            $date = (int)$date;
             $hour5 = $date - (5 * 60 * 60);
             $bot->replyText($event->getReplyToken(), '$date='.$date.'5時間前='.$hour5.'型='.gettype($date));
             $yogen = '良い';
